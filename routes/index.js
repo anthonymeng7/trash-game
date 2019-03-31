4c23d023fe3271d3535fbb9fcbe39fcc0a156ddb
ame <33564712+ame@users.noreply.github.com>
2019-03-30 23:48:47 -0700

    google cloud

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+var vision = require('C:/Users/menga/Documents/GitHub/trash3/services/gcloud-service');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', trashName: 'cookies' });
+  res.render('index', { title: 'express', trashName: 'cookies' });
+  res.send(vision.quickStart());
 });
 
 module.exports = router;
